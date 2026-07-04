1a4fc69eefd7e201ff86136c0559b34999e5e0ef
piyush gupta <piyushguptacse786@gmail.com>
2026-07-04 18:28:35 +0530

    build the register and login api

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,98 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+async function registerUser(req ,res) {
+
+    const { username, email, password, role = "user" } = req.body;
+
+    const isUserExist = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
+    if(isUserExist) {
+        return res.status(400).json({
+            message : "user already exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password,10);
+
+
+    const user = await userModel.create({
+        username,
+        email,
+        password : hash,
+        role
+    })
+    
+    const token =jwt.sign({
+        userid : user._id,
+        role : user.role
+    } , process.env.JWT_SECRET)
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message : "user registered successfully",
+        token,
+        user:{
+            id : user._id,
+            username :user.username,
+            email : user.email,
+            role : user.role,
+        }
+    
+    })
+
+
+}
+
+async function loginuser (req,res){
+
+    const { username , email , password } = req.body;
+
+    const user = await userModel.findOne({
+        $or : [{
+            username,
+            email
+        }]
+    })
+    if(!user){
+        return res.status(400).json({
+            message: " user not found "
+        })
+    }
+    
+    const isPasswordValid = await bcrypt.compare(password , user.password);
+
+    if (!isPasswordValid){
+        return res.status(400).json({
+            message : " invalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        userid: user._id,
+        role : user.role
+    }, process.env.JWT_SECRET);
+
+    res.cookie("token" , token )
+
+    res.status(200).json({
+        message : "user logged in successfully",
+        token,
+        user :{
+            id : user._id,
+            username : user.username,
+            email : user.email,
+            role : user.role
+            
+        }
+    })
+}
+module.exports = {registerUser, loginuser};
